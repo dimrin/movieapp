04c88c9eb7284b77f487cb499e4c85024083050d
Vadim Dobrin <zuka537122@gmail.com>
2022-09-26 17:15:02 +0300

    Finished

---
+++ app/src/main/java/com/vadim/movieapp/screens/home/HomeScreen.kt
@@ -0,0 +1,48 @@
+package com.vadim.movieapp.screens.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.vadim.movieapp.model.Movie
+import com.vadim.movieapp.model.getMovies
+import com.vadim.movieapp.navigation.MovieScreens
+import com.vadim.movieapp.widgets.MovieRow
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.LightGray, elevation = 5.dp) {
+            Text(text = "Movies")
+        }
+    }) {
+        MainContent(navController)
+    }
+}
+
+@Composable
+fun MainContent(
+    navController: NavController,
+    movieList: List<Movie> = getMovies()
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it) { movie ->
+                    navController.navigate(route = MovieScreens.DetailScreen.name + "/$movie")
+
+                }
+            }
+        }
+    }
+
+}
+
